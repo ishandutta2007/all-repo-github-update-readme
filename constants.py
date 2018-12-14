7f45c880e76ec24b2042c331eecae5e22a08517b
Ishan Dutta <ishandutta2007@gmail.com>
2018-12-14 16:09:39 +0530

    fixing bugs

--- constants.py
+++ constants.py
@@ -9,3 +9,4 @@ GOOG_PASS = config.get("configuration", "goog_ishandutta2007_password")
 GITHUB_ID = 'ishandutta2007'
 GITHUB_PASS = config.get("configuration", "github_ishandutta2007_password")
 GITHUB_API_TOKEN = config.get("configuration", "github_ishandutta2007_api_token")
+SEARCH_STRING = 'paypal.me/ishandutta2007'
--- github-repo-updater.py
+++ github-repo-updater.py
@@ -24,14 +24,15 @@ def update(url):
 	try:
 		r = requests.get(url)
 		repo_item = json.loads(r.text or r.content)
-		print(repo_item)
-		if repo_item['message']=='Not Found' or repo_item['message'].find("API rate limit exceeded")>=0:
-			return
-		else:
+		try:
+			if repo_item['message']=='Not Found' or repo_item['message'].find("API rate limit exceeded")>=0:
+				print(url, repo_item['message'])
+				return
+		except Exception as e:
 			pass
 		deco_content = base64.b64decode(repo_item['content']).decode('utf-8')
 
-		if deco_content.find('paypal') == -1:
+		if deco_content.find(constants.SEARCH_STRING) == -1:
 			appended_str = open('readme_md_template.txt', 'r').read()
 			deco_content = deco_content + appended_str
 			retj = {}
@@ -40,27 +41,28 @@ def update(url):
 			retj["sha"] = repo_item['sha']
 
 			# pp.pprint(retj)
+			time.sleep(1)
 
 			headers = {'Authorization': 'token %s' % constants.GITHUB_API_TOKEN}
 			r2 = requests.put(url, data = json.dumps(retj), headers=headers)
 			print(url, r2)
 		else:
-			print(url, "already has paypal")
-		time.sleep(5)
+			print(url, "already has", constants.SEARCH_STRING)
 	except Exception as e:
 		traceback.print_exc()
 		print(url, e)
 
 def main():
 	with open('url-list.csv','r') as fp:
-		url = fp.readline()
+		url = fp.readline().strip()
 		cnt = 1
 		while url:
-			url = url.replace("/{archive_format}{/ref}", "/contents/README.md")			
+			url = url.replace("/{archive_format}{/ref}", "/contents/README.md")
 			print("Repo {}: {}".format(cnt, url.strip()))
 			update(url)
 			cnt += 1
-			url = fp.readline()
+			url = fp.readline().strip()
+			time.sleep(15)
 
 if __name__ == '__main__':
-  main()
+	main()
